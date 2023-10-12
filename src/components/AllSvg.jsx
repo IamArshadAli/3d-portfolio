07f73819bff02065f23e36a00b0883606a61121c
IamArshadAli <arshadalikaldane@gmail.com>
2023-10-12 11:46:29 +0530

    initial commit

--- src/components/AllSvg.jsx
+++ src/components/AllSvg.jsx
@@ -68,8 +68,6 @@ export const LinkedIN = (props) => {
     return (
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="512"
-        height="512"
         viewBox="0 0 24 24"
         width="24"
         height="24"
